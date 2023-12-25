2ed3bfc3ee5bfd8958201e574050340e7d514cf3
morispolanco <morispolanco@gmail.com>
2023-12-25 16:20:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,28 @@
 import streamlit as st
-from streamlit_chat import message
-from bardapi import Bard
-import json
+from googlesearch import search
 
-with open('credentials.json', 'r') as f:
-    file = json.load(f)
-    token = file["token"]
+def obtener_precio_mas_bajo(query, ubicacion="Guatemala"):
+    precios = []
 
+    # Realizar una búsqueda en Google con la consulta específica
+    for j in search(query, num=5, stop=5, pause=2, tld="com", lang="en", country="GT"):
+        precios.append(j)
 
-# Function to generate the output
-def generate_response(prompt):
-    bard = Bard(token=token)
-    response = bard.get_answer(prompt)['content']
-    return response
+    return precios
 
+# Configuración de la página de Streamlit
+st.title("Consulta de Precio Más Bajo en Guatemala")
 
-# Function to receive user queries(input)
-def get_text():
-    input_text = st.text_input("Hello", "", key='input')
-    return input_text
+# Obtener la consulta del usuario
+query = st.text_input("Ingrese el nombre del producto:")
 
+if query:
+    # Obtener precios de Google
+    precios = obtener_precio_mas_bajo(query)
 
-# Title of the streamlit app
-st.title('My ChatBOT!')
-
-# to change the background of the chatbot UI page
-changes = '''
-<style>
-[data-testid="stAppViewContainer"]
-    {
-    background-image:url("https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
-    background-position: 60% 40%;
-    background-repeat: no-repeat;
-    background-size: cover;
-    }
-</style>
-'''
-st.markdown(changes, unsafe_allow_html=True)
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-# Accepting user input
-user_input = get_text()
-if user_input:
-    print(user_input)
-    output = generate_response(user_input)
-    print(output)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state['generated']:
-    # for loop is to print the chat in reverse order
-    for i in range(len(st.session_state['generated']) - 1, -1, -1):
-        # below syntax identify as every message as a unique key
-        message(st.session_state['generated'][i], key=str(i))
-        message(st.session_state['past'][i], key="user_" + str(i), is_user=True)
+    # Mostrar resultados
+    st.subheader("Resultados de la búsqueda:")
+    for precio in precios:
+        st.write(precio)
+else:
+    st.warning("Ingrese el nombre del producto para realizar la búsqueda.")
